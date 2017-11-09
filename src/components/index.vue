3e43d32d14e015272e4cde4a2896496c086ab9da
Nick Hulea <nhulea@getty.edu>
2017-11-09 15:39:45 -0800

    working ajax vue

---
+++ src/components/index.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="hello">
+    <div>User {{ $route.params.id }}</div>
+    <ul v-for="a in arr">
+    <li>
+      <span>{{a.id}}</span>
+    </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'Index',
+  data () {
+    return {
+      arr: []
+    }
+  },
+  methods: {
+    get (params) {
+      console.log(this.$route)
+      const uri = this.$route.params.id
+      return axios.get(`https://nhm.org/nature/map/map/searchobservations?params[project_id]=` + uri)
+      .then((res) => {
+        this.arr = res.data
+      })
+    }
+  },
+  created () {
+    this.get()
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,23 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import { AsyncDataPlugin } from 'vue-async-data-2'
 import HelloWorld from '@/components/HelloWorld'
+import Index from '@/components/index'
 
+Vue.use(AsyncDataPlugin)
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Hello',
-      component: HelloWorld
-    }
+  mode: 'history',
+  routes: [{
+    path: '/',
+    name: 'Hello',
+    component: HelloWorld
+  },
+  {
+    path: '/:id',
+    name: 'Index',
+    component: Index
+  }
   ]
 })
