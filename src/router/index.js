c403b6e9de2d99bcf5081d9f8ded169c3ef74ee9
Nick Hulea <nhulea@getty.edu>
2017-11-13 10:42:30 -0800

    clean up files for getty branch

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import { AsyncDataPlugin } from 'vue-async-data-2'
 import HelloWorld from '@/components/HelloWorld'
-import Index from '@/components/index'
+import Index from '@/components/Index'
 
 Vue.use(AsyncDataPlugin)
 Vue.use(Router)
